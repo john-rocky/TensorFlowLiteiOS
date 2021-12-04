6159e8fdc65cd5af86db3680ebfd6ec247f22290
john-rocky <john-rocky@hubot.com>
2021-12-05 05:42:16 +0900

    Initial Commit

---
+++ TensorFlowLiteiOSTests/TensorFlowLiteiOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  TensorFlowLiteiOSTests.swift
+//  TensorFlowLiteiOSTests
+//
+//  Created by 間嶋大輔 on 2021/12/05.
+//
+
+import XCTest
+@testable import TensorFlowLiteiOS
+
+class TensorFlowLiteiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
